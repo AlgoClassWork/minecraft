a4a7e625281bdd25f93a7615d52b1e1ec3bf4cf2
AlgoClassWork <kyxec.play@gmail.com>
2025-12-07 12:13:53 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
+from hero import Hero
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.land = Mapmanager()
         self.land.loadLand("land.txt")
+        self.hero = Hero((0, 0, 5), self.land)
 
 game = Game()
 game.run()
