e27dc969d46a312ea6d4d66d7d10eb6e74ab3a50
AlgoClassWork <kyxec.play@gmail.com>
2025-12-13 11:09:51 +0600

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,32 +1,48 @@
+key_switch_camera = 'c'
+key_switch_mode = 'z'
+key_forward = 'w'   
+key_back = 's'      
+key_left = 'a'      
+key_right = 'd'     
+key_up = 'e'      
+key_down = 'q'   
+key_turn_left = 'n'     
+key_turn_right = 'm'    
+key_build = 'b'     
+key_destroy = 'v'   
+key_savemap = 'k'
+key_loadmap = 'l'
+
 class Hero():
-    def __init__(self, pos, land):
-        self.land = land
-        self.hero = loader.loadModel('smiley')
-        self.hero.setColor(1, 0.5, 0)
-        self.hero.setScale(0.3) 
-        self.hero.setPos(pos)
-        self.hero.reparentTo(render)
-        self.cameraBind()
-        self.accept_events()
+   def __init__(self, pos, land):
+       self.land = land
+       self.hero = loader.loadModel('smiley')
+       self.hero.setColor(1, 0.5, 0)
+       self.hero.setScale(0.3)
+       self.hero.setH(180)
+       self.hero.setPos(pos)
+       self.hero.reparentTo(render)
+       self.cameraBind()
+       self.accept_events()
 
-    def cameraBind(self):
-        #base.disableMouse()
-        base.camera.reparentTo(self.hero)
-        base.camera.setPos(0, 0, 1)
-        self.cameraOn = True
+   def cameraBind(self):
+       base.disableMouse()
+       base.camera.reparentTo(self.hero)
+       base.camera.setPos(0, 0, 1.5)
+       self.cameraOn = True
 
-    def cameraUp(self):
-        pos = self.hero.getPos()
-        base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2]-3)
-        base.camera.reparentTo(render)
-        base.enableMouse()
-        self.cameraOn = False
+   def cameraUp(self):
+       pos = self.hero.getPos()
+       base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2]-3)
+       base.camera.reparentTo(render)
+       base.enableMouse()
+       self.cameraOn = False
 
-    def changeView(self):
-        if self.cameraOn:
-            self.cameraUp()
-        else:
-            self.cameraBind()
+   def changeView(self):
+       if self.cameraOn:
+           self.cameraUp()
+       else:
+           self.cameraBind()
 
-    def accept_events(self):
-        base.accept('c', self.changeView)
+   def accept_events(self):
+       base.accept(key_switch_camera, self.changeView)
