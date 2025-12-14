ffb68d693029c5f2612a627802c15d9f9f1e1225
AlgoClassWork <kyxec.play@gmail.com>
2025-12-14 14:01:36 +0600

    Update game.py

--- game.py
+++ game.py
@@ -4,13 +4,26 @@ from hero import Hero
 
 
 class Game(ShowBase):
-   def __init__(self):
-       ShowBase.__init__(self)
-       self.land = Mapmanager()
-       x,y = self.land.loadLand("land.txt")
-       self.hero = Hero((x//2,y//2,2),self.land)
-       base.camLens.setFov(90)
+    def __init__(self):
+        """Инициализация игры"""
+        super().__init__()  # Корректный вызов родительского конструктора
+
+        # Создаём менеджер карты и загружаем уровень
+        self.land = Mapmanager()
+        map_width, map_height = self.land.loadLand("land.txt")
+
+        # Создаём героя в центре карты
+        hero_start_pos = (
+            map_width // 2,
+            map_height // 2,
+            2
+        )
+        self.hero = Hero(hero_start_pos, self.land)
+
+        # Настройка камеры
+        base.camLens.setFov(90)
 
 
+# Точка входа в программу
 game = Game()
 game.run()
