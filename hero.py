328b96263310d78fa9b74cc06c4d9953f3203274
AlgoClassWork <kyxec.play@gmail.com>
2025-12-14 11:21:16 +0600

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,167 +1,91 @@
-key_switch_camera = 'c' # камера привязана к герою или нет
-key_switch_mode = 'z' # можно проходить сквозь препятствия или нет
+from panda3d.core import Vec3
 
 
-key_forward = 'w'   # шаг вперёд (куда смотрит камера)
-key_back = 's'      # шаг назад
-key_left = 'a'      # шаг влево (вбок от камеры)
-key_right = 'd'     # шаг вправо
-key_up = 'e'      # шаг вверх
-key_down = 'q'     # шаг вниз
+key_switch_camera = 'c'
 
+key_forward = 'w'
+key_back = 's'
+key_left = 'a'
+key_right = 'd'
 
-key_turn_left = 'n'     # поворот камеры направо (а мира - налево)
-key_turn_right = 'm'    # поворот камеры налево (а мира - направо)
-
-
-key_build = 'b'     # построить блок перед собой
-key_destroy = 'v'   # разрушить блок перед собой
+key_turn_left = 'n'
+key_turn_right = 'm'
 
 
 class Hero():
-   def __init__(self, pos, land):
-       self.land = land
-       self.mode = True # режим прохождения сквозь всё
-       self.hero = loader.loadModel('smiley')
-       self.hero.setColor(1, 0.5, 0)
-       self.hero.setScale(0.3)
-       self.hero.setH(180)
-       self.hero.setPos(pos)
-       self.hero.reparentTo(render)
-       self.cameraBind()
-       self.accept_events()
+    def __init__(self, pos, land):
+        self.land = land
+        self.mode = True
 
+        self.hero = loader.loadModel('smiley')
+        self.hero.setScale(0.3)
+        self.hero.setPos(pos)
+        self.hero.setH(0)
+        self.hero.reparentTo(render)
 
-   def cameraBind(self):
-       base.disableMouse()
-       # base.camera.setH(180)
-       base.camera.reparentTo(self.hero)
-       base.camera.setPos(0, 0, 1.5)
-       self.cameraOn = True
+        self.cameraBind()
+        self.accept_events()
 
+    # ---------------- КАМЕРА (FPS) ----------------
+    def cameraBind(self):
+        base.disableMouse()
+        base.camera.reparentTo(self.hero)
+        base.camera.setPos(0, 0, 1.6)   # ← В ГОЛОВЕ
+        base.camera.setHpr(0, 0, 0)
+        self.cameraOn = True
 
-   def cameraUp(self):
-       pos = self.hero.getPos()
-       base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2]-3)
-       base.camera.reparentTo(render)
-       base.enableMouse()
-       self.cameraOn = False
+    def cameraUp(self):
+        base.camera.reparentTo(render)
+        base.enableMouse()
+        self.cameraOn = False
 
+    def changeView(self):
+        if self.cameraOn:
+            self.cameraUp()
+        else:
+            self.cameraBind()
 
-   def changeView(self):
-       if self.cameraOn:
-           self.cameraUp()
-       else:
-           self.cameraBind()
+    # ---------------- ПОВОРОТ ----------------
+    def turn_left(self):
+        self.hero.setH(self.hero.getH() + 3)
 
+    def turn_right(self):
+        self.hero.setH(self.hero.getH() - 3)
 
-   def turn_left(self):
-       self.hero.setH((self.hero.getH() + 5) % 360)
+    # ---------------- ДВИЖЕНИЕ ----------------
+    def move(self, direction: Vec3):
+        self.hero.setPos(self.hero.getPos() + direction)
 
+    def forward(self):
+        self.move(self.hero.getQuat().getForward())
 
-   def turn_right(self):
-       self.hero.setH((self.hero.getH() - 5) % 360)
+    def back(self):
+        self.move(-self.hero.getQuat().getForward())
 
+    def left(self):
+        self.move(-self.hero.getQuat().getRight())
 
-   def look_at(self, angle):
-       ''' возвращает координаты, в которые переместится персонаж, стоящий в точке (x, y),
-       если он делает шаг в направлении angle'''
+    def right(self):
+        self.move(self.hero.getQuat().getRight())
 
+    # ---------------- СОБЫТИЯ ----------------
+    def accept_events(self):
+        base.accept(key_turn_left, self.turn_left)
+        base.accept(key_turn_left + '-repeat', self.turn_left)
 
-       x_from = round(self.hero.getX())
-       y_from = round(self.hero.getY())
-       z_from = round(self.hero.getZ())
+        base.accept(key_turn_right, self.turn_right)
+        base.accept(key_turn_right + '-repeat', self.turn_right)
 
+        base.accept(key_forward, self.forward)
+        base.accept(key_forward + '-repeat', self.forward)
 
-       dx, dy = self.check_dir(angle)
-       x_to = x_from + dx
-       y_to = y_from + dy
-       return x_to, y_to, z_from
+        base.accept(key_back, self.back)
+        base.accept(key_back + '-repeat', self.back)
 
+        base.accept(key_left, self.left)
+        base.accept(key_left + '-repeat', self.left)
 
-   def just_move(self, angle):
-       '''перемещается в нужные координаты в любом случае'''
-       pos = self.look_at(angle)
-       self.hero.setPos(pos)
+        base.accept(key_right, self.right)
+        base.accept(key_right + '-repeat', self.right)
 
-
-   def move_to(self, angle):
-       if self.mode:
-           self.just_move(angle)
-       else:
-           self.try_move(angle)
-  
-   def check_dir(self,angle):
-       ''' возвращает округленные изменения координат X, Y,
-       соответствующие перемещению в сторону угла angle.
-       Координата Y уменьшается, если персонаж смотрит на угол 0,
-       и увеличивается, если смотрит на угол 180.   
-       Координата X увеличивается, если персонаж смотрит на угол 90,
-       и уменьшается, если смотрит на угол 270.   
-           угол 0 (от 0 до 20)      ->        Y - 1
-           угол 45 (от 25 до 65)    -> X + 1, Y - 1
-           угол 90 (от 70 до 110)   -> X + 1
-           от 115 до 155            -> X + 1, Y + 1
-           от 160 до 200            ->        Y + 1
-           от 205 до 245            -> X - 1, Y + 1
-           от 250 до 290            -> X - 1
-           от 290 до 335            -> X - 1, Y - 1
-           от 340                   ->        Y - 1  '''
-       if angle >= 0 and angle <= 20:
-           return (0, -1)
-       elif angle <= 65:
-           return (1, -1)
-       elif angle <= 110:
-           return (1, 0)
-       elif angle <= 155:
-           return (1, 1)
-       elif angle <= 200:
-           return (0, 1)
-       elif angle <= 245:
-           return (-1, 1)
-       elif angle <= 290:
-           return (-1, 0)
-       elif angle <= 335:
-           return (-1, -1)
-       else:
-           return (0, -1)
-
-
-   def forward(self):
-       angle =(self.hero.getH()) % 360
-       self.move_to(angle)
-
-
-   def back(self):
-       angle = (self.hero.getH()+180) % 360
-       self.move_to(angle)
-  
-   def left(self):
-       angle = (self.hero.getH() + 90) % 360
-       self.move_to(angle)
-
-
-   def right(self):
-       angle = (self.hero.getH() + 270) % 360
-       self.move_to(angle)
-
-
-
-   def accept_events(self):
-       base.accept(key_turn_left, self.turn_left)
-       base.accept(key_turn_left + '-repeat', self.turn_left)
-       base.accept(key_turn_right, self.turn_right)
-       base.accept(key_turn_right + '-repeat', self.turn_right)
-
-
-       base.accept(key_forward, self.forward)
-       base.accept(key_forward + '-repeat', self.forward)
-       base.accept(key_back, self.back)
-       base.accept(key_back + '-repeat', self.back)
-       base.accept(key_left, self.left)
-       base.accept(key_left + '-repeat', self.left)
-       base.accept(key_right, self.right)
-       base.accept(key_right + '-repeat', self.right)
-
-
-       base.accept(key_switch_camera, self.changeView)
+        base.accept(key_switch_camera, self.changeView)
