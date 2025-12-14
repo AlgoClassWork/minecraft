6da74977a4c10d1fccc9d5a559b4893e68176def
AlgoClassWork <kyxec.play@gmail.com>
2025-12-14 14:53:21 +0600

    Update hero.py

--- hero.py
+++ hero.py
@@ -148,6 +148,14 @@ class Hero:
         angle = (self.hero.getH() + 270) % 360
         self.move_to(angle)
 
+    def up(self):
+        if self.mode:
+            self.hero.setZ( self.hero.getZ() + 1 )
+
+    def down(self):
+        if self.mode:
+            self.hero.setZ( self.hero.getZ() - 1 )
+
     # --------------------------
     # События клавиш
     # --------------------------
@@ -167,6 +175,11 @@ class Hero:
         base.accept(key_left + '-repeat', self.left)
         base.accept(key_right, self.right)
         base.accept(key_right + '-repeat', self.right)
+
+        base.accept(key_up, self.up)
+        base.accept(key_up + '-repeat', self.up)
+        base.accept(key_down, self.down)
+        base.accept(key_down + '-repeat', self.down)
 
         # Переключение вида камеры
         base.accept(key_switch_camera, self.changeView)
